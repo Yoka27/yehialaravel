49b4f202446bfe597530f5cedc1c5bd7a85e1305
Yehia Rashad <86153621+Yoka27@users.noreply.github.com>
2021-12-11 10:34:29 +0200

    reinstalled

--- resources/views/iti/iti.blade.php
+++ resources/views/iti/iti.blade.php
@@ -4,5 +4,4 @@
         <div class="card-body text-center">
             <img src="{{asset('storage/images/index.jpg')}}" width="700" height="500" alt="yehia" title="book image">
         </div>
-
 @endsection
